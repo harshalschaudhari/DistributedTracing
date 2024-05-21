b58920fa99249685d338e16f5812cb196fdd0612
Harshal Chaudhari <harshalschaudhari@gmail.com>
2024-05-21 13:27:10 +0530

    Pass the incomming header

--- UserRequestService/Controllers/UserRequestController.cs
+++ UserRequestService/Controllers/UserRequestController.cs
@@ -1,8 +1,6 @@
-﻿using Jaeger;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using OpenTracing;
-using System.Net.Http;
+using OpenTracing.Propagation;
 
 namespace UserRequestService.Controllers
 {
@@ -26,7 +24,28 @@ namespace UserRequestService.Controllers
             string requestUrl = $"api/Calculator/add?num1={num1}&num2={num2}";
             var actionName = ControllerContext.ActionDescriptor.DisplayName;
             using var scope = tracer.BuildSpan(actionName).StartActive(true);
+
+            Dictionary<string, IEnumerable<string>> requestHeader = Request.Headers.ToDictionary(a => a.Key, a => (IEnumerable<string>)a.Value);
+            
+            var requestHeaderDictionary = new Dictionary<string, string>();
+             
+            if (scope.Span != null)
+            {
+                foreach (var header in requestHeader)
+                {
+                    scope.Span.SetTag(header.Key, string.Join(",", header.Value.ToArray()));
+                    requestHeaderDictionary.Add(header.Key, string.Join(",", header.Value.ToArray()));
+
+                    Console.WriteLine("Request Header - " + header.Key, string.Join(",", header.Value.ToArray()));
+                }
+            }
+            
+            tracer.Inject(scope.Span.Context, BuiltinFormats.HttpHeaders, new TextMapInjectAdapter(requestHeaderDictionary));
+
             var client = httpClientFactory.CreateClient("CalculatorService");
+            foreach (var entry in requestHeaderDictionary)
+                client.DefaultRequestHeaders.Add(entry.Key, entry.Value);
+
             var response = await client.GetAsync(requestUrl);
             return response.IsSuccessStatusCode
                 ? Ok(Convert.ToDouble(await response.Content.ReadAsStringAsync()))
